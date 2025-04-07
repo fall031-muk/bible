488d8e993b517328451a5971db1b3ef3820d5458
fall031-muk <fall900802@gmail.com>
2025-04-08 08:53:17 +0900

    feat: netlify 배포

---
+++ build/precache-manifest.e1dba31e21abd051d13c735e3268e347.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbda8c57ef45c0ea35d3f6f9eb63a39a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "972ff78ca47efc8b92ea",
+    "url": "/static/css/main.afa2b338.chunk.css"
+  },
+  {
+    "revision": "eb80203d281aead42bf9",
+    "url": "/static/js/2.a17d6406.chunk.js"
+  },
+  {
+    "revision": "cebcc681b75d622a217a",
+    "url": "/static/js/3.80e6f045.chunk.js"
+  },
+  {
+    "revision": "972ff78ca47efc8b92ea",
+    "url": "/static/js/main.3c490c50.chunk.js"
+  },
+  {
+    "revision": "a86d256f6c27610337a1",
+    "url": "/static/js/runtime~main.b533b238.js"
+  }
+]);
